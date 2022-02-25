9c9826a7b45a64b8a4ca75cf038791d340937601
DanielPeterssonAI <89392459+DanielPeterssonAI@users.noreply.github.com>
2022-02-25 12:59:08 +0100

    ehhh kommer inte ihåg

--- webserver.py
+++ webserver.py
@@ -6,14 +6,14 @@ from sklearn.neighbors import KNeighborsClassifier
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
     with open("index.html") as f:
         lines = f.readlines()
 
-    return ''.join(lines)
+    return "".join(lines)
 
-@app.route('/predict', methods = ["GET", "POST"])
+@app.route("/predict", methods = ["GET", "POST"])
 def predict():
     data = str(request.get_data())[2:-2].split(",")
     data = list(map(int, data))
@@ -22,7 +22,7 @@ def predict():
     
     y_pred = model_KNN.predict(data)
 
-    return f"KNN predicted: {y_pred[0]}"
+    return f"KNN predicted:  {y_pred[0]}"
 
 def knn_predictor(x_predict, model):
     return model.predict(x_predict)
@@ -31,8 +31,8 @@ def knn_predictor(x_predict, model):
 x_train = x_train.reshape(-1, 28 * 28)
 x_test = x_test.reshape(-1, 28 * 28)
 
-model_KNN = KNeighborsClassifier(n_neighbors = 1)
+model_KNN = KNeighborsClassifier(n_neighbors = 4)
 model_KNN.fit(x_train, y_train)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
